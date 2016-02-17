7d0878f7aad722b2dbd1f8d3ddc78b9c3fc30b11
allen.mh <allen.mh@alibaba-inc.com>
2016-02-17 17:10:27 +0800

    add request function

--- index.lua
+++ index.lua
@@ -5,14 +5,15 @@ local policyset = require "greyrelease.conf.policyset"
 local transform = require "greyrelease.components.transform"
 local servermap = require "greyrelease.components.servermap"
 local logtools  = require "greyrelease.utils.log"
+local request = require "greyrelease.components.request"
 
 
 -- define the grey flow policy
-local flow_policy = policyset.grey_policy_id_hash 
+local flow_policy = policyset.grey_policy_id_range 
 
 -- define the grey flow key : request parameter, assign certain parameter 
 -- request url example : http(s)://host/?uuid=xxxx&a=b&c=d
-local hash_id = ngx.var.arg_uuid
+local hash_id = request.get_param( "uuid" )
 
 local bucket = transform.get_bucket_by_id( hash_id, flow_policy )
 
